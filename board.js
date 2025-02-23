2d40bb9817f2eb87dc7a1312eee4ddb1cdfa2b5d
hitesh <hiteshtripathi91@gmail.com>
2025-02-23 17:28:43 +0530

    range added for inner div

--- board.js
+++ board.js
@@ -1,26 +1,30 @@
 const sizes = document.getElementsByClassName('sizes')
 const board = document.getElementById('board')
+const size = document.getElementById('size')
+const rangename = document.getElementById('rangename')
 let trues = 0
-Array.from(sizes).forEach(element => {
-    element.addEventListener('click',()=>{
-        board.innerHTML=''
-        let c = Number(element.textContent)
-        let b = Math.floor(400/c)
-        for(let i=0;i<c*c;i++){
-        const inner = document.createElement('div')
-        inner.className = 'inner'
-        inner.style.width = `${b}px`
-        inner.style.height = `${b}px`
-        inner.style.backgroundColor='white'
-        board.appendChild(inner)
 
-
-        }
-
-    })
+    size.addEventListener('click',()=>sizeselector(size.value))
    
-    
-});
+ 
+function sizeselector(value){
+    board.innerHTML=''
+    let c = Number(value)
+    let b = (500/c)
+    for(let i=0;i<c*c;i++){
+    const inner = document.createElement('div')
+    inner.className = 'inner'
+    inner.style.width = `${b}px`
+    inner.style.height = `${b}px`
+    inner.style.backgroundColor='white'
+    board.appendChild(inner)
+   }
+   rangename.textContent=`${c} x ${c}`
+
+
+}    
+sizeselector(size.value)
+
 let colour = '#000000'
 const scolor = document.getElementById('colourpicker')
 scolor.addEventListener('input',()=>{
